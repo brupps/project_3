0ad6816c019b38c5078685ca8923b40e39682a8c
brupps <barbrupps@gmail.com>
2024-02-27 21:36:29 -0600

    added tornado alley json layer to heat map

--- static/heat.js
+++ static/heat.js
@@ -5,9 +5,16 @@ let base = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 });
 
+let night = L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+      subdomains: 'abcd',
+      maxZoom: 20
+  });
+
 let url = "https://raw.githubusercontent.com/brupps/project_3/main/static/data.geojson";
 
-let tornadoAlley = "static/tornadoAlley.geojson";
+let tornadoAlley = "https://raw.githubusercontent.com/brupps/project_3/main/static/tornadoAlley.geojson";
+
 
  // Create a baseMap to hold the base and add to map.
  let baseMaps = {
@@ -16,15 +23,31 @@ let tornadoAlley = "static/tornadoAlley.geojson";
 
 //Create overlaymap to hold earthquake locations.
 let overlayMaps = {
-  "Tornado Alley": tornadoAlley,
+  "Night": night,
 };
 
 let myMap = L.map("map", {
   center: [39.8283, -98.5795],
   zoom: 4,
-  layers: [base, tornadoAlley]
+  layers: [base]
   });
 
+
+d3.json(tornadoAlley).then((geodata)=> {
+  let alley = L.geoJson(geodata,{
+    style: {
+      color: "red",
+      opacity: .5,
+      fill: true
+    }
+  });
+  layerControl.addOverlay(alley, 'Tornado Alley');
+});
+
+ let layerControl = L.control.layers(baseMaps,overlayMaps,
+  {collapsed: false,}).addTo(myMap);
+
+  
 d3.json(url).then(function(response) {
 
   console.log(response);
